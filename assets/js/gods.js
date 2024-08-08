91760eec193f06d48b5fbbb2a9886cd6e0362a6c
Alejandro Botero <alejandro@albotero.com>
2024-08-08 12:37:17 -0500

    First commit

---
+++ assets/js/gods.js
@@ -0,0 +1,91 @@
+/* 
+  https://es.wikipedia.org/wiki/Dioses_ol%C3%ADmpicos
+  https://in.pinterest.com/pin/545639311120121776/
+*/
+
+export const GODS = [
+  {
+    id: 1,
+    greek: "Zeus",
+    roman: "Júpiter",
+    desc: "Rey de los dioses y gobernante del monte Olimpo; dios del cielo y el trueno. Hijo menor de los titanes Crono y Rea. Los símbolos incluyen el rayo, el águila, el roble, el cetro y la balanza. Hermano y marido de Hera, aunque tuvo muchas amantes, tanto diosas como mortales.",
+    image: { x: "6", y: "47" },
+  },
+  {
+    id: 2,
+    greek: "Hera",
+    roman: "Juno",
+    desc: "Reina de los dioses y diosa del matrimonio y la familia. Hija menor de Crono y Rea. Esposa y hermana de Zeus. Los símbolos incluyen el pavo real, la granada, la corona, el cuco, el león y la vaca. Siendo la diosa del matrimonio, con frecuencia trata de vengarse de los amantes de Zeus y sus hijos.",
+    image: { x: "97.5", y: "87" },
+  },
+  {
+    id: 3,
+    greek: "Poseidón",
+    roman: "Neptuno",
+    desc: "Señor de los mares, los terremotos y los caballos. Los símbolos incluyen el caballo, el toro, el delfín y el tridente. Medio hijo de Crono y Rea. Hermano de Zeus y Hades. Casado con la nereida Anfítrite, aunque, como la mayor parte de dioses masculinos griegos, tuvo amantes de ambos sexos.",
+    image: { x: "65.5", y: "48" },
+  },
+  {
+    id: 4,
+    greek: "Apolo",
+    roman: "Apolo o Febo",
+    desc: "Dios de la música y las bellas artes, de la luz, el sol, la medicina, la belleza, el conocimiento, la poesía, la profecía y el tiro con arco. Los símbolos incluyen el sol, la lira, el arco y la flecha, el cuervo, el delfín, el lobo, el cisne y el ratón. Hijo menor de Zeus y Leto.nota 1​Hermano gemelo de Artemisa. Líder de las musas inspiradoras. Tuvo muchas amantes, tanto diosas como mortales, y una extensa lista de hijos.",
+    image: { x: "94", y: "12" },
+  },
+  {
+    id: 5,
+    greek: "Atenea",
+    roman: "Minerva",
+    desc: "Virgen diosa de la sabiduría, la artesanía, la defensa y la guerra estratégica. Los símbolos incluyen la lechuza y el olivo. Hija de Zeus y de la oceánide Metis, surgida de la cabeza de su padre totalmente adulta y con armadura de combate completa después de que este se hubiera tragado a su madre.",
+    image: { x: "38", y: "86" },
+  },
+  {
+    id: 6,
+    greek: "Afrodita",
+    roman: "Venus",
+    desc: "Diosa del amor, la belleza y el deseo. Los símbolos incluyen la paloma, el pájaro, la manzana, la abeja, el cisne, el mirto y la rosa. Su nombre proviene del griego αφρο 'afro', surgida de la espuma del mar después de que la sangre de Urano goteara sobre la tierra y el mar tras ser castrado por su hijo Crono. En otros mitos se la señala como hija de Zeus y de la oceánide Dione. Casada con Hefesto, aunque tuvo muchas relaciones con sus amantes, en especial con Ares (de quien tuvo ocho hijos, entre ellos Eros). Su nombre nos dio la palabra «afrodisíaco».nota 2​, y de su equivalente en romano antiguo (Venere), provienen venerar y venérea.",
+    image: { x: "37", y: "49" },
+  },
+  {
+    id: 7,
+    greek: "Ares",
+    roman: "Marte",
+    desc: "Dios de la guerra, la fuerza, la virilidad masculina, y el derramamiento de sangre. Protector del olimpo y de los ejércitos. Los símbolos son la lanza y el escudo. También los son la antorcha, el jabalí, la serpiente, el perro, el buitre. Hijo de Zeus y Hera, amante preferido de Afrodita, su aliada en la guerra y sanadora. Su nombre romano Marte, nos dio la palabra marcial, martes (día de la semana) y el mes de marzo.",
+    image: { x: "68", y: "87" },
+  },
+  {
+    id: 8,
+    greek: "Dioniso",
+    roman: "Baco",
+    desc: "Dios del vino, las celebraciones y el éxtasis. Dios patrón del arte del teatro. Los símbolos incluyen la vid, la hiedra, la copa, el tigre, la pantera, el leopardo, el delfín y la cabra. Hijo de Zeus y de la mortal princesa de Tebas Sémele. Casado con la princesa cretense Ariadna. El olímpico más joven, así como el único nacido de una mujer mortal.",
+    image: { x: "31", y: "12" },
+  },
+  {
+    id: 9,
+    greek: "Hermes",
+    roman: "Mercurio",
+    desc: "Mensajero de los dioses; dios del comercio y la retórica. Los símbolos incluyen el caduceo (vara entrelazada con dos serpientes), las sandalias y el casco alados, la cigüeña y la tortuga (cuyo caparazón usó para inventar la lira). Hijo de Zeus y la ninfa Maia. El segundo olímpico más joven, apenas mayor que Dioniso. Se casó con Dríope, hija del rey Dríope, y su hijo Pan se convirtió en el dios de la naturaleza, el señor de los sátiros, el inventor de la flauta y el compañero de Dioniso.",
+    image: { x: "65", y: "11.5" },
+  },
+  {
+    id: 10,
+    greek: "Artemisa",
+    roman: "Diana",
+    desc: "Diosa virgen de la caza, la virginidad, el parto, el tiro con arco y todos los animales. Los símbolos incluyen la luna, el ciervo, el perro de caza, la osa, la serpiente, el ciprés y el arco y la flecha. Hermana gemela de Apolo. Hija mayor de Zeus y Leto.",
+    image: { x: "3.5", y: "11" },
+  },
+  {
+    id: 11,
+    greek: "Hades",
+    roman: "Plutón",
+    desc: "Dios del inframundo, de los muertos y las riquezas de la tierra («Pluto» se traduce como «el rico»), nació en la primera generación olímpica, pero debido a que vive en el inframundo en vez de en el monte Olimpo, suele no ser incluido entre los doce olímpicos. Robó a Perséfone de la Tierra y la convirtió en su esposa en el inframundo, de donde la dejaba salir cada seis meses para reunirse con su madre.",
+    image: { x: "7", y: "85" },
+  },
+  {
+    id: 12,
+    greek: "Deméter",
+    roman: "Ceres",
+    desc: "Diosa de la fertilidad, la agricultura, la naturaleza y las estaciones del año. Los símbolos incluyen la amapola, el trigo, la antorcha y el cerdo. Medio hija de Crono y Rea. Su nombre latino, Ceres, nos dio la palabra «cereal».",
+    image: { x: "96", y: "51" },
+  },
+]
---
+++ assets/js/load.js
@@ -0,0 +1,39 @@
+import { GODS } from "./gods.js"
+
+const menuIcon = document.querySelector(".menu-icon")
+const galleryDiv = document.querySelector(".gallery")
+const infoDiv = document.querySelector(".info")
+let mobileMenuShown = false
+
+menuIcon.addEventListener("click", () => {
+  mobileMenuShown = !mobileMenuShown
+  menuIcon.classList.remove("active")
+  if (mobileMenuShown) menuIcon.classList.add("active")
+})
+
+infoDiv.addEventListener("click", () => (infoDiv.style.display = "none"))
+
+const showGodInfo = ({ greek, roman, desc, image: { x, y } }) => {
+  infoDiv.innerHTML = `
+  <div class="info-container">
+    <div class="god-img" style="background-position: ${x}% ${y}%;"></div>
+    <h4 class="greek-title">Nombre griego:</h4>
+    <p class="greek-text">${greek}</p>
+    <h4 class="roman-title">Nombre romano:</h4>
+    <p class="roman-text">${roman}</p>
+    <p class="desc-text">${desc}</p>
+  </div>`
+  infoDiv.style.display = "block"
+}
+
+// Load Gods' data
+GODS.forEach((el) => {
+  const {
+    image: { x, y },
+  } = el
+  const god = document.createElement("div")
+  god.className = "god-img"
+  god.style.backgroundPosition = `${x}% ${y}%`
+  god.addEventListener("click", () => showGodInfo(el))
+  galleryDiv.appendChild(god)
+})
